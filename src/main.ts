29c79b7d229d0913f2eb0e9fe9f40441d0f7fd7a
Shreyansh Dubey <shreyansh2128@gmail.com>
2025-06-13 14:58:42 +0530

    final add and edited added

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import "primeicons/primeicons.css";
 import router from './router';
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 const app = createApp(App)
 app.use(router)
+app.use(Toast)
 app.mount('#app')
--- src/views/EditJobView.vue
+++ src/views/EditJobView.vue
@@ -78,7 +78,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  <section class="bg-green-50">
+  <section class="bg-blue-50">
     <div class="container m-auto max-w-2xl py-24">
       <div
         class="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"

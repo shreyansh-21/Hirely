6350ff7cfed1aa0527ab7007ef90e04db8d00bbc
Shreyansh Dubey <shreyansh2128@gmail.com>
2025-06-13 13:50:05 +0530

    Add & Editjobview back button , proxy routes update

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,32 +3,44 @@ import {createRouter , createWebHistory} from 'vue-router';
 import JobsView from '@/views/JobsView.vue';
 import JobView from '@/views/JobView.vue';
 import NotFoundView from '@/views/NotFoundView.vue';
+import AddJobView from '@/views/AddJobView.vue';
+import EditJobView from '@/views/EditJobView.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-        path: '/',
-        name: 'home',
-        component: HomeView
+      path: '/',
+      name: 'home',
+      component: HomeView,
     },
     {
-        path: '/jobs',
-        name: 'JobsView',
-        component: JobsView
+      path: '/jobs',
+      name: 'jobs',
+      component: JobsView,
     },
     {
-        path: '/jobs/:id',
-        name: 'Job',
-        component: JobView
+      path: '/jobs/:id',
+      name: 'job',
+      component: JobView,
+    },
+    {
+      path: '/jobs/add',
+      name: 'add-job',
+      component: AddJobView,
+    },
+    {
+      path: '/jobs/edit/:id',
+      name: 'edit-job',
+      component: EditJobView,
     },
     {
       path: '/:catchAll(.*)',
-      name: 'NotFound',
-      component: NotFoundView
-    }
-    
-  ]
+      name: 'not-found',
+      component: NotFoundView,
+    },
+  ],
 });
-export default router;
 
+export default router;
